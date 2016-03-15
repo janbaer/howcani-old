34a7ec86444911f4bbe62661ca051edca650f185
Jan Baer <jan@janbaer.de>
2016-03-15 21:57:15 +0100

    Add routing

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,13 +1,16 @@
 'use strict';
 
 import { Component } from 'angular2/core';
+import { RouteConfig } from 'angular2/router';
 import { Navbar } from './components/navbar/navbar.component.js';
+import { router } from './router.component.js';
 
 @Component({
   selector: 'howcani-app',
   templateUrl: 'app/app.tpl.html',
   directives: [Navbar]
 })
+@RouteConfig(router.config)
 export class AppComponent {
   constructor() {
     this.title = 'How can I do this';
