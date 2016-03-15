2c17e84fed2ca1830e71e8230b9fa5a33b033066
Jan Baer <jan@janbaer.de>
2016-03-15 21:43:57 +0100

    Create Navbar component

---
+++ src/app/components/navbar/navbar.component.js
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './app/components/navbar/navbar.tpl.html'
+})
+export class Navbar {
+  constructor() {
+  }
+}
