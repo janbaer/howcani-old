f3c1c81fa18f54aee61d5cef874d44bf33fee6ba
Omar Diego Vera Ustariz <diego.ustariz@check24.de>
2016-03-16 16:52:09 +0100

    Solves issue #7 Add OAuth

--- src/app/components/navbar/navbar.component.js
+++ src/app/components/navbar/navbar.component.js
@@ -1,10 +1,34 @@
 import { Component } from 'angular2/core';
+import { Provider, Request } from '@zalando/oauth2-client-js'
 
 @Component({
   selector: 'navbar',
   templateUrl: './app/components/navbar/navbar.tpl.html'
 })
 export class Navbar {
+
   constructor() {
+  }
+
+  loginUsingGithub() {
+    console.log('Logging using github!!!');
+
+    var github = new Provider({
+      id: 'github',
+      authorization_url: 'https://github.com/login/oauth/authorize'
+    });
+
+    console.log('Requesting the token');
+
+    var request = new Request({
+      client_id: '22b411052b6b16a65c8d',
+      redirect_uri: 'http://localhost:3000/'
+    });
+
+    var uri = github.requestToken(request);
+
+    github.remember(request);
+
+    window.location.href = uri;
   }
 }
