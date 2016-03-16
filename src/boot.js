18757fc1da066c2a5c30f909c3a96c33c898a659
Jan Baer <jan@janbaer.de>
2016-03-16 14:03:36 +0100

    Update package.json versions and reconfigure boot.js and webpack

--- src/boot.js
+++ src/boot.js
@@ -1,17 +1,19 @@
 'use strict';
 
 import './shim';
-import 'reflect-metadata';
-import 'zone.js/lib/browser/zone-microtask';
+import 'rxjs/Rx';
+
+//import 'reflect-metadata';
+//import 'zone.js/lib/browser/zone-microtask';
+
 import { bootstrap } from 'angular2/platform/browser';
+
 import { AppComponent } from './app/app.component';
 import { enableProdMode, provide } from 'angular2/core';
 
-if (ENVIRONMENT === 'production') {
-  enableProdMode();
-}
+//if (ENVIRONMENT === 'production') {
+  //enableProdMode();
+//}
 
-bootstrap(AppComponent, [
-
-]);
+bootstrap(AppComponent, []);
 
