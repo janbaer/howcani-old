6ef31272b1964b6a6e927f0dd0394b55f64f74ba
Jan Baer <jan@janbaer.de>
2016-03-21 18:41:32 +0100

    Fix a typo in webpack.config.prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 
-webPackConfig.plugins = [
+webpackConfig.plugins = [
   new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js"),
   new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }}),
   new webpack.DefinePlugin({ ENVIRONMENT: JSON.stringify('production') })
