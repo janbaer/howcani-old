374a12f9cabcb656736424be930ea9b0f800bf26
Jan Baer <jan@janbaer.de>
2016-03-27 10:41:50 +0200

    Implement validation for Github user and project

--- src/app/components/connect/connect.component.js
+++ src/app/components/connect/connect.component.js
@@ -1,39 +1,62 @@
 import { Component } from 'angular2/core';
 import { Router } from 'angular2/router';
+import { FORM_DIRECTIVES, FormBuilder, Validators } from 'angular2/common';
 import { Configuration } from './../../services/configuration.service.js';
 import { DataService } from './../../services/data.service.js';
-import { ToastService } from './../../services/toast.service.js';
 
 @Component({
   selector: 'connect',
-  templateUrl: './app/components/connect/connect.tpl.html'
+  templateUrl: './app/components/connect/connect.tpl.html',
+  directives: [FORM_DIRECTIVES]
 })
 export class ConnectComponent {
   constructor(
               router: Router,
+              formBuilder: FormBuilder,
               configuration: Configuration,
-              dataService: DataService,
-              toastService: ToastService
+              dataService: DataService
              ) {
     this.router = router;
     this.dataService = dataService;
     this.configuration = configuration;
-    this.toastService = toastService;
 
-    this.project = Object.assign({}, this.configuration.project);
+    this.connectForm = formBuilder.group({
+      user: [this.configuration.project.user, Validators.required, this.validateUser.bind(this)],
+      repository: [this.configuration.project.repository, Validators.required, this.validateRepository.bind(this)]
+    });
+
+    this.connectForm.controls['user'].valueChanges
+      //.debounceTime(200) // It's now working in the current version, maybe some later
+      .subscribe({
+        next: () => {
+          this.connectForm.controls['repository'].updateValueAndValidity();
+        }
+      });
+  }
+
+  validateUser(control) {
+    return this.dataService.validate(control.value)
+      .then((isValid) => {
+        // The validation result have to be in a positive case null
+        return isValid ? null : { isValid: false };
+      });
+  }
+
+  validateRepository(control) {
+    const user = this.connectForm ? this.connectForm.controls['user'].value : undefined;
+
+    return this.dataService.validate(user, control.value)
+      .then((isValid) => {
+        return isValid ? null : { isValid: false };
+      });
   }
 
   onSubmit() {
-    this.isBusy = true;
-    this.dataService.verify(this.project)
-      .then((isValid) => {
-        if (isValid) {
-          this.configuration.project = this.project;
-          this.router.navigate(['Questions']);
-        } else {
-          this.isBusy = false;
-          this.toastService.show('The given Github user or project does not exists! Please verify your input!');
-        }
-      });
+    this.configuration.project = {
+      user: this.connectForm.controls['user'].value,
+      repository: this.connectForm.controls['repository'].value
+    };
+
+    this.router.navigate(['Questions']);
   }
 }
--- src/app/services/data.service.js
+++ src/app/services/data.service.js
@@ -8,20 +8,16 @@ export class DataService {
     this.configuration = configuration;
   }
 
-  verify(project) {
+  validate(user, repository) {
     return new Promise((resolve) => {
       // TODO: Verify that the github user and repository exists
       setTimeout(() => {
-        const isValid = project !== undefined;
+        let isValid = user === 'howcani-project';
+        if (repository) {
+          isValid = isValid && repository === 'howcani-data';
+        }
         resolve(isValid);
       }, 1000);
-    });
-  }
-
-  connect() {
-    return new Promise((resolve) => {
-      // TODO: here we could fetch the latest questions
-      resolve();
     });
   }
 }
