3b9af458afcc5d2194bc4d6b661bfc101f4d76cc
Jan Baer <jan@janbaer.de>
2016-03-28 17:49:56 +0200

    Change implementation for Github validation
    It's now again just in the moment when the user clicks ok.
    The problem was that the validation was triggered with each input
    and this raises a lot of 403 or 404 errors till the user has entered
    the complete user and repository name

--- src/app/components/connect/connect.component.js
+++ src/app/components/connect/connect.component.js
@@ -1,8 +1,9 @@
 import { Component } from 'angular2/core';
 import { Router } from 'angular2/router';
-import { FORM_DIRECTIVES, FormBuilder, Validators } from 'angular2/common';
+import { FORM_DIRECTIVES, FormBuilder } from 'angular2/common';
 import { Configuration } from './../../services/configuration.service.js';
 import { QuestionService } from './../../services/question.service.js';
+import { ToastService } from './../../services/toast.service.js';
 
 @Component({
   selector: 'connect',
@@ -14,50 +15,30 @@ export class ConnectComponent {
               router: Router,
               formBuilder: FormBuilder,
               configuration: Configuration,
-              questionService: QuestionService
+              questionService: QuestionService,
+              toastService: ToastService
              ) {
     this.router = router;
     this.questionService = questionService;
     this.configuration = configuration;
+    this.toastService = toastService;
 
-    this.connectForm = formBuilder.group({
-      user: [this.configuration.project.user, Validators.required, this.validateUser.bind(this)],
-      repository: [this.configuration.project.repository, Validators.required, this.validateRepository.bind(this)]
-    });
-
-    this.connectForm.controls['user'].valueChanges
-      //.debounceTime(200) // It's now working in the current version, maybe some later
-      .subscribe({
-        next: () => {
-          this.connectForm.controls['repository'].updateValueAndValidity();
-        }
-      });
-  }
-
-  validateUser(control) {
-    return this.questionService.validate(control.value)
-      .then((isValid) => {
-        // The validation result have to be in a positive case null
-        return isValid ? null : { isValid: false };
-      });
-  }
-
-  validateRepository(control) {
-    const user = this.connectForm ? this.connectForm.controls['user'].value :
-                                    this.configuration.project.user;
-
-    return this.questionService.validate(user, control.value)
-      .then((isValid) => {
-        return isValid ? null : { isValid: false };
-      });
+    this.project = Object.assign({}, this.configuration.project);
   }
 
   onSubmit() {
-    this.configuration.project = {
-      user: this.connectForm.controls['user'].value,
-      repository: this.connectForm.controls['repository'].value
-    };
+    this.isBusy = true;
 
-    this.router.navigate(['Questions']);
+    this.questionService.validate(this.project.user, this.project.repository)
+      .then((isValid) => {
+        this.isBusy = false;
+        if (isValid) {
+          this.configuration.project = this.project;
+          this.router.navigate(['Questions']);
+        } else {
+          this.toastService.show('The given Github user or project does not exists! Please verify your input!');
+        }
+      });
+
   }
 }
