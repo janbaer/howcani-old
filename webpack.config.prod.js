b818983e92a1b32a86217fefc858910b7dcf5e28
Jan Baer <jan@janbaer.de>
2016-03-30 06:01:25 +0200

    Reactivate the minification of JS in prod build with different settings

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ const webpackConfig = require('./webpack.config.js');
 
 webpackConfig.plugins = [
   new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js"),
-  //new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }}),
+  new webpack.optimize.UglifyJsPlugin({ minimize: true, mangle: false, comments: false }),
   new webpack.DefinePlugin({ ENVIRONMENT: JSON.stringify('production') })
 ];
 
