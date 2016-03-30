5872b3174eea86410c51b1cdaf41d83b72eaf019
Jan Baer <jan@janbaer.de>
2016-03-30 18:05:27 +0200

    Show questions that will be fetched from github

--- src/app/components/questions/questions.component.js
+++ src/app/components/questions/questions.component.js
@@ -1,12 +1,38 @@
 import { Component } from 'angular2/core';
 import { Configuration } from './../../services/configuration.service.js';
+import { QuestionService } from './../../services/question.service.js';
 
 @Component({
   selector: 'questions',
   templateUrl: './app/components/questions/questions.tpl.html'
 })
 export class QuestionsComponent {
-  constructor(configuration: Configuration) {
+  constructor(configuration: Configuration, questionService: QuestionService) {
     this.project = configuration.project;
+    this.questionService = questionService;
+
+    //this.questions = [];
+
+  }
+
+  getQuestions() {
+    return this.questionService.getAllQuestions();
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    this.questions = this.questionService.fetchQuestions();
+    //this.questionService.fetchQuestions()
+      //.then((questions) => {
+        //console.log('Questions', questions);
+        //this.questions = questions;
+      //})
+      //.catch((err) => {
+        //console.log('Error while fetching issues', err);
+      //});
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
   }
 }
