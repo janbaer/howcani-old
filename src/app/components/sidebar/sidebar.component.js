1be6e430bcd2790ef8bcd15b53fb675a4f91947c
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2016-04-06 15:24:10 +0200

    Add initial sidebar component, and basic styling

---
+++ src/app/components/sidebar/sidebar.component.js
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'sidebar',
+  templateUrl: './app/components/sidebar/sidebar.tpl.html',
+  directives: []
+})
+export class SidebarComponent {
+  constructor() {
+  }
+}
