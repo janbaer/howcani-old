e51fcde516db23e2ad0f5681675b19165c064440
Jan Baer <jan@janbaer.de>
2016-04-16 17:27:32 +0200

    Use GithubService in LabelService

--- src/app/components/sidebar/sidebar.component.js
+++ src/app/components/sidebar/sidebar.component.js
@@ -43,7 +43,6 @@ export class SidebarComponent {
   }
 
   ngOnInit() {
-    console.log('here');
-    //this.labelService.fetchLabels();
+    this.labelService.fetchLabels();
   }
 }
--- src/app/services/label.service.js
+++ src/app/services/label.service.js
@@ -1,37 +1,20 @@
 import { Injectable } from 'angular2/core';
-import { Http, RequestOptions, URLSearchParams } from 'angular2/http';
-import { Observable } from 'rxjs/Observable';
-import { ConfigurationService } from './configuration.service.js';
+import { GithubService } from './github.service';
 
 @Injectable()
 export class LabelService {
   labels = [];
 
-  constructor(configuration: ConfigurationService, http: Http) {
-    this.configuration = configuration;
-    this.http = http;
+  constructor(githubService: GithubService) {
+    this.githubService = githubService;
   }
 
   fetchLabels() {
-    let url = `https://api.github.com/repos/${this.configuration.project.user}/${this.configuration.project.repository}/labels`;
-
-    if (this.configuration.oauthToken) {
-      url += `?access_token=${this.configuration.oauthToken}`;
-    }
-
     this.labels = [];
 
-    const searchParams = new URLSearchParams();
-    const requestOptions = new RequestOptions({ search: searchParams });
-
-    return this.http.get(url, requestOptions)
-             .map((response) => response.json())
-             .catch((error) => {
-               console.log('Error while featching data');
-               return Observable.throw(error.json().error || 'Github error');
-             })
+    return this.githubService.getLabels()
              .subscribe((labels) => {
-               this.labels = labels.map((label) => label.name);
+               this.labels = labels;
              });
   }
 }
