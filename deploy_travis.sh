d84ae3e09d7616e281484ad0f1edb09c1c6c13da
Jan Baer <jan@janbaer.de>
2016-06-26 11:23:40 +0200

    Change deployment with travis

--- deploy_travis.sh
+++ deploy_travis.sh
@@ -1,17 +1,18 @@
-if [[ "$TRAVIS_TAG" == "" ]]; then
+VERSION=$1
+GITHUB_TOKEN=$2
+
+if [[ "$VERSION" == "" ]]; then
   echo "We do not have a tag"
   exit 0
 fi
 
-git config --global user.name "TRAVIS-CI"
-git config --global user.email "jan@janbaer.de"
-git clone https://janbaer:${GH_TOKEN}@github.com/howcani-project/howcani-project.github.io.git dist
+git clone https://janbaer:${GITHUB_TOKEN}@github.com/howcani-project/howcani-project.github.io.git dist
 cd dist
 git rm -r .
 cp -r ../build/* ./
 rm ./scripts/*.js.map
 rm ./styles/*.css.map
 git add -u && git add .
-git commit -m "Version ${TRAVIS_TAG}"
+git commit -m "Version ${VERSION}"
 git push origin master
 
