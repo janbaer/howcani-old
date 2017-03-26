e7b226d2fa27551d650e06fef649eac03429bcbd
Jan Baer <jan@janbaer.de>
2017-03-26 17:34:37 +0200

    Fix missing scripts for ServiceWorker support

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 webpackConfig.plugins = [
   new webpack.ContextReplacementPlugin(
@@ -11,7 +12,11 @@ webpackConfig.plugins = [
   ),
   new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename:"vendor.js"}),
   new webpack.optimize.UglifyJsPlugin({ minimize: true, mangle: false, comments: false }),
-  new webpack.DefinePlugin({ ENVIRONMENT: JSON.stringify('production') })
+  new webpack.DefinePlugin({ ENVIRONMENT: JSON.stringify('production') }),
+  new CopyWebpackPlugin([
+    { from: './src/service-worker.js', to: path.resolve(__dirname, './build') },
+    { from: './node_modules/sw-toolbox/sw-toolbox.js', to: path.resolve(__dirname, './build/scripts/sw-toolbox.js') }
+  ])
 ];
 
 module.exports = webpackConfig;
